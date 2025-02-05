5a5fdfbfffefb704ad0c9ff5d22e336b1f94561a
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-05 14:30:35 -0300

    chore(producer): config RMQ on app module

--- producer/src/app.module.ts
+++ producer/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "ORDER_SERVICE", //Nome do token de injeção para enviar mensagens ou gerar eventos
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'orders-queue'
+        }
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
